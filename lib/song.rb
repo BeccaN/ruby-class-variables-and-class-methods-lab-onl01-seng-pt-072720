49bbc024612e055331d5289b8811f956f06408cc
Rebecca Congi <rncongi@gmail.com>
2020-08-14 16:41:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,7 @@ class Song
     genre_hash = {}
     @@genres.each do |genre|
       if genre_hash.has_key?(genre)
-        genre.value += 1 
+        genre_hash[genre] += 1 
       else
         genre_hash[genre] = 1 
       end
